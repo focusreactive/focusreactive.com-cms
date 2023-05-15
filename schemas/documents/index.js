bde037a5627bd8bc3a90262b3dd240d15f1a48c1
AlexHramovich <alex.gramovich.1996@gmail.com>
2023-05-15 16:51:55 +0200

    add our work page

--- schemas/documents/index.js
+++ schemas/documents/index.js
@@ -1,5 +1,6 @@
 import mainSchemas from './main';
 import landingSchemas from './landing';
 import footer from './footer';
+import ourWork from './ourWork';
 
-export default [...mainSchemas, ...landingSchemas, ...footer];
+export default [...mainSchemas, ...landingSchemas, ...ourWork, ...footer];
