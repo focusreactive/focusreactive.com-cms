af36462f2a2f5e4be3dfb16175a003af6246237d
AlexHramovich <alex.gramovich.1996@gmail.com>
2023-06-01 15:28:14 +0200

    add about us page to cms

--- schemas/documents/index.js
+++ schemas/documents/index.js
@@ -2,5 +2,12 @@ import mainSchemas from './main';
 import landingSchemas from './landing';
 import footer from './footer';
 import ourWork from './ourWork';
+import aboutUsPage from './aboutUs';
 
-export default [...mainSchemas, ...landingSchemas, ...ourWork, ...footer];
+export default [
+  ...mainSchemas,
+  ...landingSchemas,
+  ...ourWork,
+  ...footer,
+  ...aboutUsPage,
+];
