fdfb0246e10297c112f2913ca30b81a8a73f2b56
Eugene Boruhov <eugene@focusreactive.com>
2023-09-06 18:07:13 +0200

    Show author name instead of id in subtitle

--- schemas/blog/post.ts
+++ schemas/blog/post.ts
@@ -89,13 +89,19 @@ const post = {
   preview: {
     select: {
       title: 'title',
-      authors: 'authors',
-      media: 'heroImage',
+      author0: 'authors.0.name',
+      author1: 'authors.1.name',
+      author2: 'authors.2.name',
+      author3: 'authors.3.name',
     },
-    prepare(selection) {
-      const { authors } = selection;
-      const author = authors[0]._ref;
-      return { ...selection, subtitle: author && `by ${author}` };
+    prepare: ({ title, author0, author1, author2, author3 }) => {
+      const authors = [author0, author1, author2].filter(Boolean);
+      const subtitle = authors.length > 0 ? `by ${authors.join(', ')}` : '';
+      const hasMoreAuthors = Boolean(author3);
+      return {
+        title,
+        subtitle: hasMoreAuthors ? `${subtitle}…` : subtitle,
+      };
     },
   },
 };
