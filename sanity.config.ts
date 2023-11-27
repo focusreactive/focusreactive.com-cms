7dd9275cfe813eb8a14542b3dd8a7a79362d547d
Eugene Boruhov <eugene@focusreactive.com>
2023-11-27 18:27:39 +0100

    Add media plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,7 @@ import { schemaTypes } from './schemas';
 import { markdownSchema } from 'sanity-plugin-markdown';
 import { defaultDocumentNode } from './structure';
 import { Logo } from './components/Logo';
+import { media } from 'sanity-plugin-media';
 
 export default defineConfig({
   name: 'fr-website',
@@ -21,6 +22,7 @@ export default defineConfig({
       defaultApiVersion: '2021-10-21',
     }),
     markdownSchema(),
+    media(),
   ],
 
   schema: {
