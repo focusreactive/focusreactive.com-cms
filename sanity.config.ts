f14760750230e1cef8de085efba00425a438ce67
Oleg Proskurin <regx@usul.su>
2024-01-26 22:17:09 +0700

    Update config

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,10 +4,11 @@ import { visionTool } from '@sanity/vision';
 import { lighthousePlugin } from 'sanity-lighthouse-plugin';
 import { schemaTypes } from './schemas';
 import { markdownSchema } from 'sanity-plugin-markdown';
-import { defaultDocumentNode } from './structure';
+import { defaultDocumentNode, structure } from './structure';
 import { Logo } from './components/Logo';
 import { media } from 'sanity-plugin-media';
 import { RebuildPreview } from './actions/TriggerPreviewRebuild';
+import { apiVersion } from './settings';
 
 export default defineConfig({
   name: 'fr-website',
@@ -19,9 +20,10 @@ export default defineConfig({
   plugins: [
     deskTool({
       defaultDocumentNode,
+      structure,
     }),
     visionTool({
-      defaultApiVersion: '2021-10-21',
+      defaultApiVersion: apiVersion,
     }),
     lighthousePlugin(),
     markdownSchema(),
---
+++ settings.ts
@@ -0,0 +1,1 @@
+export const apiVersion = '2021-10-21';
