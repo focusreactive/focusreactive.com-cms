d1e70b0a974acfeb477c2d2f8db7c8e3d7512e88
Eugene Boruhov <eugene@focusreactive.com>
2024-01-28 18:53:17 +0100

    Add status bar and actions to preview panel

---
+++ components/PreviewIfame.tsx
@@ -0,0 +1,133 @@
+import { SanityDocument } from '@sanity/types';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
+import { Box, Button, Card, Flex, Spinner, Text, ThemeProvider } from '@sanity/ui';
+import { UserViewComponent } from 'sanity/desk';
+import { LaunchIcon, RefreshIcon } from '@sanity/icons';
+
+const MAIN_PREVIEW_URL = 'https://fr-11ty-migration-front.vercel.app';
+const BLOG_PREVIEW_URL = 'https://focusreactivecom-blog-preview.vercel.app';
+
+const getPreviewUrl = (document: Partial<SanityDocument>) => {
+  if (document._type === 'post') {
+    return getBlogPagePreviewUrl(document);
+  }
+
+  return getMainPagePreviewUrl(document);
+};
+
+const getMainPagePreviewUrl = (document: Partial<SanityDocument>) => {
+  const documentSlug = document?.path?.current;
+  let previewSlug = '';
+
+  switch (document._type) {
+    case 'landingPage': {
+      previewSlug = `landing-preview?slug=${documentSlug}`;
+      break;
+    }
+    case 'aboutUsPage': {
+      previewSlug = 'preview?slug=about-preview';
+      break;
+    }
+    case 'mainPage': {
+      previewSlug = 'preview?slug=main-preview';
+      break;
+    }
+    case 'ourWorkPage': {
+      previewSlug = 'preview?slug=our-work-preview';
+      break;
+    }
+    default: {
+      previewSlug = 'default-preview';
+      break;
+    }
+  }
+
+  return `${MAIN_PREVIEW_URL}/api/${previewSlug}`;
+};
+
+const getBlogPagePreviewUrl = (document: Partial<SanityDocument>) => {
+  const slug = document?.slug?.current;
+  return `${BLOG_PREVIEW_URL}/${slug}`;
+};
+
+export const PreviewIframe: UserViewComponent = ({ document }) => {
+  const iframe = useRef<HTMLIFrameElement>(null);
+  const [previewUrl, setPreviewUrl] = useState('');
+  const [id, setId] = useState(1);
+
+  const { displayed: currentDocument } = document;
+
+  function reloadIframe() {
+    if (!iframe?.current) return;
+    setId(id + 1);
+  }
+  const debouncedChangeHandler = useCallback(debounce(reloadIframe, 500), []);
+
+  useEffect(() => {
+    if (debouncedChangeHandler) {
+      debouncedChangeHandler();
+    }
+  }, [currentDocument._updatedAt, debouncedChangeHandler]);
+
+  useEffect(() => {
+    setPreviewUrl(getPreviewUrl(currentDocument));
+  }, []);
+
+  if (!previewUrl)
+    return (
+      <ThemeProvider>
+        <Flex padding={5} align="center" justify="center">
+          <Spinner />
+        </Flex>
+      </ThemeProvider>
+    );
+
+  return (
+    <ThemeProvider>
+      <Flex direction="column" style={{ height: `100%` }}>
+        <Card paddingX={4} paddingY={3} borderBottom>
+          <Flex align="center" gap={2}>
+            <Box flex={1}>
+              <Text size={1} textOverflow="ellipsis" >
+                {previewUrl}
+              </Text>
+            </Box>
+            <Flex align="center" gap={1}>
+              <Button
+                fontSize={[1]}
+                padding={2}
+                icon={RefreshIcon}
+                title="Reload"
+                text="Reload"
+                aria-label="Reload"
+                onClick={reloadIframe}
+              />
+
+              <Button
+                fontSize={[1]}
+                icon={LaunchIcon}
+                padding={[2]}
+                text="Open"
+                tone="primary"
+                onClick={() => window.open(previewUrl)}
+              />
+            </Flex>
+          </Flex>
+        </Card>
+        <Card tone="transparent" padding={0} style={{ height: `100%` }}>
+          <Flex align="center" justify="center" style={{ height: `100%` }}>
+            <iframe
+              key={id}
+              ref={iframe}
+              title="preview"
+              style={{ width: '100%', height: `100%`, maxHeight: `100%`, border: 0 }}
+              src={previewUrl}
+              referrerPolicy="origin-when-cross-origin"
+            />
+          </Flex>
+        </Card>
+      </Flex>
+    </ThemeProvider>
+  );
+};
--- structure.tsx
+++ structure.tsx
@@ -1,104 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import debounce from 'lodash.debounce';
 import { DefaultDocumentNodeResolver } from 'sanity/desk';
-import { SanityDocument } from '@sanity/types';
-
-const MAIN_PREVIEW_URL = 'https://fr-11ty-migration-front.vercel.app';
-const BLOG_PREVIEW_URL = 'https://focusreactivecom-blog-preview.vercel.app';
-
-const sendPostMessage = () => {
-  const iframe = document.getElementById('preview_iframe') as HTMLIFrameElement | null;
-
-  if (!iframe || !iframe.contentWindow) return null;
-
-  iframe.contentWindow.postMessage(
-    'reload()',
-    iframe.src.includes(BLOG_PREVIEW_URL) ? BLOG_PREVIEW_URL : MAIN_PREVIEW_URL,
-  );
-};
-
-const getPreviewUrl = (document: Partial<SanityDocument>) => {
-  if (document._type === 'post') {
-    return getBlogPagePreviewUrl(document);
-  }
-
-  return getMainPagePreviewUrl(document);
-};
-
-const getMainPagePreviewUrl = (document: Partial<SanityDocument>) => {
-  const documentSlug = document?.path?.current;
-  let previewSlug = '';
-
-  switch (document._type) {
-    case 'landingPage': {
-      previewSlug = `landing-preview?slug=${documentSlug}`;
-      break;
-    }
-    case 'aboutUsPage': {
-      previewSlug = 'preview?slug=about-preview';
-      break;
-    }
-    case 'mainPage': {
-      previewSlug = 'preview?slug=main-preview';
-      break;
-    }
-    case 'ourWorkPage': {
-      previewSlug = 'preview?slug=our-work-preview';
-      break;
-    }
-    default: {
-      previewSlug = 'default-preview';
-      break;
-    }
-  }
-
-  return `${MAIN_PREVIEW_URL}/api/${previewSlug}`;
-};
-
-const getBlogPagePreviewUrl = (document: Partial<SanityDocument>) => {
-  const slug = document?.slug?.current;
-  return `${BLOG_PREVIEW_URL}/${slug}`;
-};
-
-const JsonPreview = ({
-  document: sanityDocument,
-}: {
-  document: {
-    draft: SanityDocument | null;
-    displayed: Partial<SanityDocument>;
-    historical: Partial<SanityDocument> | null;
-    published: SanityDocument | null;
-  };
-}) => {
-  const [previewUrl, setPreviewUrl] = useState('');
-
-  const debouncedChangeHandler = useCallback(debounce(sendPostMessage, 1000), []);
-
-  useEffect(() => {
-    if (debouncedChangeHandler) {
-      debouncedChangeHandler();
-    }
-  }, [sanityDocument?.displayed?._updatedAt, debouncedChangeHandler]);
-
-  useEffect(() => {
-    setPreviewUrl(getPreviewUrl(sanityDocument?.displayed));
-  }, []);
-
-  return (
-    <iframe
-      title="page"
-      id="preview_iframe"
-      src={previewUrl}
-      style={{ height: '100%', width: '100%', border: 'none' }}
-    />
-  );
-};
+import { PreviewIframe } from './components/PreviewIfame';
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, { schemaType }) => {
   const documentsWithPreview = ['landingPage', 'aboutUsPage', 'mainPage', 'ourWorkPage', 'post'];
 
   if (documentsWithPreview.includes(schemaType)) {
-    return S.document().views([S.view.form(), S.view.component(JsonPreview).title('Preview')]);
+    return S.document().views([S.view.form(), S.view.component(PreviewIframe).title('Preview')]);
   }
   return S.document();
 };
